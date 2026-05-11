1c70096b46ce56d79405bbdefc7f75283468f503
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-05-11 21:17:23 +0600

    Selling Sandwiches

---
+++ python/Selling_Sandwiches.py
@@ -0,0 +1,20 @@
+import math
+from collections import deque, Counter, defaultdict
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify
+import sys
+
+if 'LOCAL' in sys.argv:
+    sys.stdin = open('input.txt', 'r') 
+    sys.stdout = open('output.txt', 'w') 
+      
+      
+def solve():
+    x, y, z = map(int, sys.stdin.readline().split())
+    
+    a = y + z
+    profit = x - a
+    print(profit)
+    
+if __name__ == "__main__":
+    solve()
