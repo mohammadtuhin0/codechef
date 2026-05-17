7e484bf67b0ad5e57c0599b598ab2cfcc1ef9a65
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-05-17 15:12:26 +0600

    Constraints

---
+++ Constraints.py
@@ -0,0 +1,16 @@
+import math
+from collections import deque, Counter, defaultdict
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop, heapify
+import sys
+
+if 'LOCAL' in sys.argv:
+    sys.stdin = open('input.txt', 'w')
+    sys.stdout = open('output.txt', 'r')
+    
+def solve():
+    x = int(sys.stdin.readline())
+    multi = 2 * x
+    print(multi)
+    
+solve()
