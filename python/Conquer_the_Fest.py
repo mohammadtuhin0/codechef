2645f34663efc12c7dc9cff8b20888a936dde325
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-05-20 23:16:32 +0600

    Conquer the Fest!!

---
+++ python/Conquer_the_Fest.py
@@ -0,0 +1,19 @@
+import math
+from collections import deque, Counter, defaultdict
+from heapq import heappush, heappop, heapify
+from bisect import bisect_right, bisect_left
+import sys
+
+if 'LOCAL' in sys.argv:
+    sys.stdin = open("input.txt", "w")
+    sys.stdout = open("output.txt", "r")
+    
+def solve():
+    N, B = map(int, sys.stdin.readline().split())
+    
+    if N == B * 10:
+        print("YES")
+    else:
+        print("NO")
+        
+solve()
