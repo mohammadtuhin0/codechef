4a0cdf1240384633c6541d995ff9e1225971f35d
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-05-28 22:24:33 +0600

    Time Penalty

--- python/Time_Penalty.py
+++ python/Time_Penalty.py
@@ -5,8 +5,8 @@ from heapq import heappush, heappop, heapify
 import sys
 
 if 'LOCAL' in sys.argv:
-    sys.stdin = open('input.txt', 'w')
-    sys.stdout = open('output.txt', 'r')
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
     
 def solve():
     x, y = map(int, sys.stdin.readline().split())
