62941c83b8bf0294e0f0b5ad7c9fed7cbb39a5ac
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-08-03 19:31:54 +0600

    Buying Chairs

---
+++ Buying_Chairs.py
@@ -0,0 +1,26 @@
+import sys
+
+def solve():
+    input = sys.stdin.read 
+    data = input().split()
+    
+    T = int(input[0])
+    idx = 1
+    
+    results = []
+    
+    for _ in range(T):
+        W = int(data[idx])
+        P = int(data[idx+1])
+        K = int(data[idx+2])
+        idx += 3
+        
+        if K <= W:
+            results.append(str(2 * K))
+        else:
+            results.append(str(W + K))
+            
+    print('\n'.join(results))
+    
+if __name__ == '__main__':
+    solve()
